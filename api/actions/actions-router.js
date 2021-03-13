933d62403cf4714cb0dc70eff7d20185f2e9044b
hdm007 <harry.mardirossian13@gmail.com>
2021-03-12 19:23:05 -0500

    added the actions logic

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const {validateAction, validateActionContent} = require("./actions-middleware")
 // Write your "actions" router here!
 
 const Action = require("./actions-model")
@@ -6,19 +7,35 @@ const Action = require("./actions-model")
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-
+    Action.get()
+    .then( actions => {
+        res.json(actions)
+    })
+    .catch(next)
 })
-router.get("/:id", (req, res) => {
-
+router.get("/:id", validateAction, (req, res) => {
+    res.json(req.action)
 })
 router.post("/", (req, res, next) => {
-
+    Action.insert(req.body)
+    .then(action => {
+        res.status(201).json(action);
+    })
+    .catch(next)
 })
-router.put("/:id", (req, res, next) => {
-
+router.put("/:id", validateAction, validateActionContent, (req, res, next) => {
+    Action.update(req.params.id, req.body)
+    .then(action => {
+        res.status(200).json(action)
+    })
+    .catch(next)
 })
-router.delete("", (req, res, next) => {
-
+router.delete("/:id", validateAction, (req, res, next) => {
+    Action.remove(req.params.id)
+    .then(action => {
+        res.status(200).json(action)
+    })
+    .catch(next);
 });
 
 module.exports = router;
