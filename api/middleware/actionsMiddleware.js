283c0a64f67d04493782aa3850825a0a0d9e6e1f
hdm007 <harry.mardirossian13@gmail.com>
2021-03-12 19:05:08 -0500

    endpoint shells setup, middleware shells, still need to import middleware, attach logic

---
+++ api/middleware/actionsMiddleware.js
@@ -0,0 +1,14 @@
+const Action = require("../actions/actions-model");
+
+const validateAction = async (req, res, next) => {
+
+}
+
+const validateActionContent = (req, res, next) => {
+
+}
+
+module.exports = {
+    validateAction,
+    validateActionContent
+}
---
+++ api/middleware/projectMiddleware.js
@@ -0,0 +1,15 @@
+const Project = require("../projects/projects-model");
+
+
+const validateProject = async (req, res, next) => {
+
+}
+
+const validateProjectContent = (req, res, next) => {
+
+}
+
+module.exports = {
+    validateProject,
+    validateProjectContent
+}
